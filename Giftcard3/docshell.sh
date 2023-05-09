602453e2e6063dc9edd2b72d279764865052d549
Zod Mint <abheesta@gmail.com>
2023-05-09 13:56:39 -0700

    GF2&3

---
+++ Giftcard3/docshell.sh
@@ -0,0 +1,3 @@
+docker build -t nyuappsec/assign3:v0 .
+docker build -t nyuappsec/assign3-proxy:v0 proxy/
+docker build -t nyuappsec/assign3-db:v0 db/
---
+++ Giftcard3/kubeshell.sh
@@ -0,0 +1,3 @@
+kubectl apply -f db/k8
+kubectl apply -f GiftcardSite/k8
+kubectl apply -f proxy/k8
